87fc9c0098d2c5cc6c21a2108e981848f11a12b2
Joaquin <joaquinandresdelrio@gmail.com>
2024-07-12 18:00:12 -0300

    12-7

--- src/components/ExerciseSwap.jsx
+++ src/components/ExerciseSwap.jsx
@@ -3,30 +3,24 @@ import styles from "../styles/ExerciseSwap.module.css"
 
 function ExerciseSwap({ session, n, t, setPage, setScrolled }) {
     const length = session.exerciseList.length
+    const lengthArray = Array.from({length}, (_, i) => i)
     const [exerciseN, setExerciseN] = useState(0)
-    const [animation, setAnimation] = useState(styles.fadeInLeft)
 
     const onLeft = () => {
-        setAnimation(styles.fadeOutLeft)
-        setTimeout(() => {
-            setExerciseN(index => {
-                if(index === 0) return length - 1
-                return index - 1
-            })
-            setAnimation(styles.fadeInRight)
-        }, 250)
+        setExerciseN(index => {
+            if(index === 0) return length - 1
+            return index - 1
+        })
+        console.log(exerciseN)
     }
 
     const onRight = () => {
-        setAnimation(styles.fadeOutRight)
-        setTimeout(() => {
-            setExerciseN(index => {
-                if(index === (length - 1)) return 0
-                return index + 1
-            })
-            setAnimation(styles.fadeInLeft)
-        }, 250)
+        setExerciseN(index => {
+            if(index === (length - 1)) return 0
+            return index + 1
+        })
     }
+    
 
     const onBack = () => {
         setScrolled(true)
@@ -46,33 +40,37 @@ function ExerciseSwap({ session, n, t, setPage, setScrolled }) {
                         <i className="fa-solid fa-arrow-left fa-2xl" style={{ color: '#ffffff' }}></i>
                     </div>
                 </div>
-                <div className={`${styles.card} ${styles.exerciseCard} ${animation}`}>
-                    <h1>{(t(session.exerciseList[exerciseN].exercise.name).charAt(0).toUpperCase() + t(session.exerciseList[exerciseN].exercise.name).slice(1))}</h1>
-                    <div className={styles.itemContainer}>
-                        <div className={styles.iconContainer}>
-                            <img src="images/strength.png" alt="" />
+                <div className={`${styles.card} ${styles.exerciseCard}`}>
+                    {lengthArray.map((i) => (
+                        <div className={styles.contentContainer} key={i} style={{translate: `${exerciseN *  -100}%`}}>
+                            <h1>{(t(session.exerciseList[i].exercise.name).charAt(0).toUpperCase() + t(session.exerciseList[i].exercise.name).slice(1))}</h1>
+                            <div className={styles.itemContainer}>
+                                <div className={styles.iconContainer}>
+                                    <img src="images/strength.png" alt="" />
+                                </div>
+                                {session.exerciseList[i].exercise.targetedMuscles.map((muscle, i) => (
+                                    <span key={i}>
+                                        {(t(muscle).charAt(0).toUpperCase() + t(muscle).slice(1)) + ((i + 1 === session.exerciseList[i].exercise.targetedMuscles.length) ? "" : ", ")}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className={styles.itemContainer}>
+                                <div className={styles.iconContainer}>
+                                    <i className="fa-solid fa-clipboard-question fa-xl" style={{ color: '#0989ff' }}></i>
+                                </div>
+                                {session.exerciseList[i].reps} {t("reps")}
+                            </div>
+                            <div className={`${styles.itemContainer} ${styles.dumbbell}`}>
+                                <div className={styles.iconContainer}>
+                                    <i className="fa-solid fa-dumbbell fa-xl" style={{ color: '#0989ff' }}></i>
+                                </div>
+                                {session.exerciseList[i].sets > 1 ? `${session.exerciseList[i].sets} sets` : `${session.exerciseList[i].sets} set`}
+                            </div>
+                            <div className={styles.imgContainer}>
+                                <img src={`images/${session.exerciseList[i].exercise.image}`} alt="" />
+                            </div>
                         </div>
-                        {session.exerciseList[exerciseN].exercise.targetedMuscles.map((muscle, i) => (
-                            <span key={i}>
-                                {(t(muscle).charAt(0).toUpperCase() + t(muscle).slice(1)) + (i + 1 === session.exerciseList[exerciseN].exercise.targetedMuscles.length ? "" : ", ")}
-                            </span>
-                        ))}
-                    </div>
-                    <div className={styles.itemContainer}>
-                        <div className={styles.iconContainer}>
-                            <i className="fa-solid fa-clipboard-question fa-xl" style={{ color: '#0989ff' }}></i>
-                        </div>
-                        {session.exerciseList[exerciseN].reps} {t("reps")}
-                    </div>
-                    <div className={`${styles.itemContainer} ${styles.dumbbell}`}>
-                        <div className={styles.iconContainer}>
-                            <i className="fa-solid fa-dumbbell fa-xl" style={{ color: '#0989ff' }}></i>
-                        </div>
-                        {session.exerciseList[exerciseN].sets > 1 ? `${session.exerciseList[exerciseN].sets} sets` : `${session.exerciseList[exerciseN].sets} set`}
-                    </div>
-                    <div className={styles.imgContainer}>
-                        <img src={`images/${session.exerciseList[exerciseN].exercise.image}`} alt="" />
-                    </div>
+                    ))}
                 </div>
                 <div className={styles.rightButtons}>
                     <div className={styles.exerciseButton} style={{ backgroundColor: '#0989ff', opacity: 1 }}>
